a8b95de560872cb925976d9737bbbddb65e126b8
Nikolai Gavriushin <gna@pm.me>
2025-09-30 01:06:51 +0400

    feat: enable checking, unchecking, and deleting todos

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,15 @@ import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
 
-console.log("test index.js");
+const todosList = document.querySelector(".todos__list");
+
+const generateTodo = (data) => {
+  const todo = new Todo(data, "#todo-template");
+  const todoElement = todo.getView();
+  return todoElement;
+}
+
+initialTodos.forEach((item) => {
+  const todo = generateTodo(item);
+  todosList.append(todo);
+});
